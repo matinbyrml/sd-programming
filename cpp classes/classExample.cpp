e65cf3191c59e3e35eaf3e59faac49f01d5be0ef
Matin Bayramli <matinbayramli9@gmail.com>
2026-03-24 22:46:06 +0100

    feat: cpp class

---
+++ cpp classes/classExample.cpp
@@ -0,0 +1,43 @@
+/*
+    In C++, struct and class are practically identical, with one massive difference: 
+    If you don't type an access specifier, a struct makes everything public by default, 
+    while a class makes everything private by default.
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Sensor{
+    private:
+        int id;
+        int* dataHistory; //dynamic array
+    public: 
+        Sensor(int id, int size){ // constructor, like in java
+            this->id = id;
+            dataHistory = (int*)malloc(size * sizeof(int)); //use malloc to allocate memory
+            cout << "Sensor is constructed" << endl;
+        }
+        
+        int getId(){
+            return this->id;
+        }
+        
+        // Deconsturctor: Why use it? If your constructor uses malloc or new to grab dynamic memory, 
+        // your destructor MUST contain the free or delete command to prevent memory leaks.
+        ~Sensor() {
+            free(dataHistory);
+            cout << "Sensor is destroyed from the memory" << endl;
+        }
+};
+
+int main(){
+    // Lets create an instance inside a scope:
+    {
+     Sensor s1(1, 10);
+    }
+    // Now object is out of scope; deconstructor is triggered
+
+    return 0;
+}
