4fa94e6c3ce9460c2cb6dca81e9b66d36e5c87b7
Matin Bayramli <matinbayramli9@gmail.com>
2026-03-26 19:41:31 +0100

    feat: add vector methods

--- cpp libraries/sequential containers/containers.cpp
+++ cpp libraries/sequential containers/containers.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
     // We look at vector now
-    vector<string> vault;
+    vector<string> vault; // vector size is flexible like a dynamic array
     // It creates a temporary, invisible std::string object, grabs dynamic memory on the Heap, and copies 'G', 'o', 'l', 'd' into it.
     vault.push_back("Gold"); // 
     vault.emplace_back("Diamonds"); // build diamond directly inside
@@ -29,7 +29,14 @@ int main(){
     
     vault.erase(vault.begin());
     cout << "After erase Vector size is: " << vault.size() << endl;
-    cout << "First item: " << vault.front() << endl; // Silver
-    cout << "Last item: " << vault.back() << endl;   // Diamonds
-    cout << "Middle item: " << vault.at(1) << endl;  // Gold
+    cout << "First item: " << vault.front() << endl; // Gold
+    cout << "Last item: " << vault.back() << endl;   // Gold
+    // cout << "Middle item: " << vault.at(1) << endl;  // error 
+
+    for (auto i: vault){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    vault.clear(); // Nukes everything
 }
